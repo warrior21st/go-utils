62f6ec5b01d4aae3480b4c247835cfac53666b69
warrior21st <123767168@qq.com>
2021-06-01 12:05:51 +0800

    Create string_util.go

---
+++ stringutil/string_util.go
@@ -0,0 +1,31 @@
+package stringutil
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+type StringBuilder struct {
+	buf bytes.Buffer
+}
+
+func NewStringBuilder(s ...string) *StringBuilder {
+	sb := StringBuilder{}
+	sb.buf.WriteString(strings.Join(s, ""))
+	return &sb
+}
+
+func (sb *StringBuilder) Append(s string) *StringBuilder {
+	sb.buf.WriteString(s)
+	return sb
+}
+
+func (sb *StringBuilder) AppendFormat(f string, s ...interface{}) *StringBuilder {
+	sb.buf.WriteString(fmt.Sprintf(f, s...))
+	return sb
+}
+
+func (sb *StringBuilder) String() string {
+	return sb.buf.String()
+}
