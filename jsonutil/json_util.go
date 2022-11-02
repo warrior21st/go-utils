57320447940e562f0a4a6a59ea3c7f4a10b3f8ab
warr <123767168@qq.com>
2022-11-03 01:47:28 +0800

    111

--- jsonutil/json_util.go
+++ jsonutil/json_util.go
@@ -2,6 +2,7 @@ package jsonutil
 
 import (
 	"encoding/json"
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -28,48 +29,47 @@ func ReadJsonValueFromJsonString(json string, keys string) string {
 func ReadJsonValFromDecodedBytes(f interface{}, keys string) string {
 	val := ""
 	ftemp := f
+	ok := true
 	keysArr := strings.Split(keys, ":")
 	l := len(keysArr)
 	for i := 0; i < l; i++ {
 		m := ftemp.(map[string]interface{})
 		if i < l-1 {
-			ftemp = m[keysArr[i]]
+			ftemp, ok = m[keysArr[i]]
+			if !ok {
+				panic(fmt.Errorf("can not found key '%s' from appsettings", keys))
+			}
 		} else {
+			if _, ok = m[keysArr[i]]; !ok {
+				panic(fmt.Errorf("can not found key '%s' from appsettings", keys))
+			}
+
 			switch m[keysArr[i]].(type) {
 			case bool:
 				val = strconv.FormatBool(m[keysArr[i]].(bool))
-				break
 			case int:
 				val = strconv.FormatInt(int64(m[keysArr[i]].(int)), 10)
-				break
 			case int32:
 				val = strconv.FormatInt(int64(m[keysArr[i]].(int32)), 10)
-				break
 			case int64:
 				val = strconv.FormatInt(m[keysArr[i]].(int64), 10)
-				break
 			case float32:
 				val = strconv.FormatFloat(float64(m[keysArr[i]].(float32)), 'f', -1, 64)
-				break
 			case float64:
 				val = strconv.FormatFloat(m[keysArr[i]].(float64), 'f', -1, 64)
-				break
 			case string:
 				val = m[keysArr[i]].(string)
-				break
 			default:
 				jbytes, err := json.Marshal(m[keysArr[i]])
 				if err != nil {
 					panic(err)
 				}
 				return string(jbytes)
-				//fmt.Printf("unexpected type %T", t) // %T prints whatever type t has
-				//break
 			}
 		}
 	}
 
-	return string(val)
+	return val
 }
 
 func Serialize(val *interface{}) *string {
--- settingutil/setting_util.go
+++ settingutil/setting_util.go
@@ -16,7 +16,10 @@ var (
 //获取指定设置，嵌套key使用":"分隔，如"AppSettings:DBConnectionString"
 func GetAppSetting(keys string) string {
 	if _settingsCache == nil {
-		json.Unmarshal(commonutil.ReadFileBytes(GetSettingFilePath()), &_settingsCache)
+		err := json.Unmarshal(commonutil.ReadFileBytes(GetSettingFilePath()), &_settingsCache)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	return jsonutil.ReadJsonValFromDecodedBytes(_settingsCache, keys)
